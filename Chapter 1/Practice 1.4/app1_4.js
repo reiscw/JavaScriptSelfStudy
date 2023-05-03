37a87dd6e21d1dfebc5df61c4bf947cbeefa73ae
Christopher Reis <107897047+reiscw@users.noreply.github.com>
2023-05-03 16:57:10 -0500

    Chapter 1 Work

---
+++ Chapter 1/Practice 1.4/app1_4.js
@@ -0,0 +1,9 @@
+let a = 10;
+// the preceding statement sets the variable a equal to 10
+alert(a);
+
+/* Here we see a multiple line comment
+ * Note that I changed this exercise to use an alert message
+ * instead of console.log() so that I do not need to access
+ * the console feature of the browser.*/
+
