15a65c1f4e03cb005d97392fe31c4e096902cab0
Christopher Reis <107897047+reiscw@users.noreply.github.com>
2023-05-03 16:55:54 -0500

    Delete Chapter 1 directory

--- Chapter 1/Project/Chapter1Project.js
+++
@@ -1,8 +0,0 @@
-console.log("Christopher Reis");
-
-/* Self-check question 1: <script type="text/javascript" src="name_of_file.js">
- * Self-check question 2: no, you cannot (if you try, it will display the file as text)
- * Self-check question 3: by using the /* operator to start the block and the terminating operator (seen below) to close it
- * Self-check question 4: use a single line comment (//) to comment it out
- */
- 
